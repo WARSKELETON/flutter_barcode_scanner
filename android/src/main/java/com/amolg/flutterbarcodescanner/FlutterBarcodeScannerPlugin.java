37795feae9c871dbc83b35862bdca895d2d55bf2
Lucas Vicente <yugi.man22@gmail.com>
2020-12-09 01:23:02 +0000

    Update FlutterBarcodeScannerPlugin.java

--- android/src/main/java/com/amolg/flutterbarcodescanner/FlutterBarcodeScannerPlugin.java
+++ android/src/main/java/com/amolg/flutterbarcodescanner/FlutterBarcodeScannerPlugin.java
@@ -39,8 +39,7 @@ import io.flutter.embedding.engine.plugins.lifecycle.FlutterLifecycleAdapter;
 public class FlutterBarcodeScannerPlugin implements MethodCallHandler, ActivityResultListener, StreamHandler, FlutterPlugin, ActivityAware {
     private static final String CHANNEL = "flutter_barcode_scanner";
 
-    private static 
-        activity;
+    private static FlutterFragmentActivity activity;
     private static Result pendingResult;
     private Map<String, Object> arguments;
 
